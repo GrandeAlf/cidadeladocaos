fd9e35361acb09fe9d0e5982160e174945e89a8d
Eric Moreira <eric.gmoreira98@gmail.com>
2019-02-22 17:27:54 -0300

    Tentativa fracassada de fazer funcionar o bootstrap

---
+++ php/view/inicial.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>Template Bootstrap</title>
+  
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="c:/wamp/www/cidadeladocaos/css/bootstrap.min.css" >
+  <!--<link href="css/style.css" rel="stylesheet">-->
+</head>
+<body>
+  <h1>Hello, Bootstrap!</h1>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-link">Link</button>
+
+  <!-- jQuery (necessario para os plugins Javascript do Bootstrap) -->
+  <script src="c:/wamp/www/cidadeladocaos/js/jquery.js"></script>
+  <script src="c:/wamp/www/cidadeladocaos/js/bootstrap.min.js"></script>
+</body>
+</html>
